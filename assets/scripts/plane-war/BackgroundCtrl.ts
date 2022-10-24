d954406217117ae08b7ff83fc17e721fed3cb79b
hsd <51134642@qq.com>
2022-10-24 19:20:23 +0800

    palne-war

---
+++ assets/scripts/plane-war/BackgroundCtrl.ts
@@ -0,0 +1,30 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('BackgroundCtrl')
+export class BackgroundCtrl extends Component {
+
+    private symbol: Boolean = true
+
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        if (this.symbol) {
+            for (let bgItem of this.node.children) {
+                bgItem.setPosition(0, bgItem.getPosition().y - 600 * deltaTime, 0)
+                
+                console.log(bgItem.getPosition().y);
+                
+                if (bgItem.getPosition().y <= -1275) {
+                    bgItem.setPosition(0, bgItem.getPosition().y + 1278 *2)
+                }
+            }
+
+           
+        }
+        
+    }
+}
+
